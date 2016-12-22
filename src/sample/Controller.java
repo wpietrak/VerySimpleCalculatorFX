ef11c95558d70f85bdb234049bc4f97e0d792a12
wpietrak <wojciechpietrak@gmail.com>
2016-12-22 12:01:46 +0100

    first commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,54 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class Controller {
+
+    @FXML
+    private Text output;
+    private long number1 = 0;
+    private String operator = "";
+
+    private boolean start = true;
+
+    private Model model = new Model();
+
+    @FXML
+    public void clickedButton(ActionEvent actionEvent) {
+        if (start) {
+            output.setText("");
+            start = false;
+        }
+        String value = ((Button)actionEvent.getSource()).getText();
+        output.setText(output.getText()+ value);
+    }
+
+    @FXML
+    public void doOnAction(ActionEvent actionEvent) {
+        String value = ((Button)actionEvent.getSource()).getText();
+
+        if (!"=".equals(value)){
+            if (!operator.isEmpty())
+                return;
+
+            operator = value;
+            number1 = Long.parseLong(output.getText());
+            output.setText("");
+        }
+        else {
+            if (operator.isEmpty())
+                return;
+            output.setText(String.valueOf(model.calculate(number1, Long.parseLong(output.getText()), operator )));
+            operator= "";
+            start = true;
+
+
+        }
+    }
+
+
+}
+
---
+++ src/sample/Model.java
@@ -0,0 +1,20 @@
+package sample;
+
+public class Model {
+    public long calculate (long number1, long number2, String operator ){
+        switch (operator){
+            case "+":
+                return number1+number2;
+            case "-":
+                return number1-number2;
+            case "*":
+                return number1*number2;
+            case "/":
+                if (number2==0)
+                    return 0;
+                return number1/number2;
+        }
+        System.out.print("Nieznany operator "+operator );
+        return 0;
+    }
+}
